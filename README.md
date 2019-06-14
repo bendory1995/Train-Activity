d486877245e048b3927de3e49467b60d36a3da5a
Alex Oh <alexhboh@gmail.com>
2019-06-14 09:51:50 -0400

    Added the Read Me page and the comments for the code

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a Train Schedule Program.
+User can type in the train name, destination, first time, and frequency.
+Then the computer will compute what time the next train will be and how long a user has to wait for the next train.
+The train name, first time, destination, and frequency will be stored in firebase for future reference.
+When user goes on to the webpage, the stored data will use the current time to compute the next train arrival time and the wait.
+
+On the table, it will show you the live data for train schedule.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+
+//bringing firebase info.
 var firebaseConfig = {
     apiKey: "AIzaSyBNnCDfmJnbE30ywERRrTDHIHlD9eKL9ds",
     authDomain: "traintime-8e8d3.firebaseapp.com",
@@ -10,9 +12,10 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+//saving the database to a variable.
 var database = firebase.database();
 
-
+// creating variables needed to store and compute 
 var trainName;
 var destination;
 var frequency;
@@ -21,32 +24,44 @@ var timeAway;
 var currentTime;
 var firstTimeConverted;
 
+//when a user click the submit button
 $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
+    //this takes the user input for the train's first time.
     var firstTime = $("#train-time-input").val().trim();
+    //this takes the user input for the train's frequency
     frequency = $("#train-frequency-input").val().trim();
     console.log(frequency);
+    //this takes the user input for the name of the train
     trainName = $("#train-name-input").val().trim();
+    //this takes the user input for train's destination
     destination = $("#train-destination-input").val().trim();
 
+    //this converts the time given to previous year
     firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
+    // saving current time
     currentTime = moment();
+    // calculating the difference in time between current time and the first train time in minutes
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
+    //this is getting the remainder of the time 
     var tRemainder = diffTime % frequency;
     console.log(tRemainder);
 
+    // time away is frequency minus remainder of the time.
     timeAway = frequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + timeAway);
 
+    // add time away from current time to get the next arrival time
     nextArrival = moment().add(timeAway, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
 
 
+    //creating an object with different values
     var newTrain = {
         name: trainName,
         firstTime: firstTime,
@@ -57,6 +72,7 @@ $("#add-train-btn").on("click", function (event) {
     database.ref().push(newTrain);
 
 
+    // resetting the whole form to ""
     $("#train-time-input").val("");
     $("#train-frequency-input").val("");
     $("#train-name-input").val("");
@@ -64,20 +80,28 @@ $("#add-train-btn").on("click", function (event) {
 
 });
 
-database.ref().on("child_added",function(childSnapshot){
+
+    //when the data is added on firebase
+    database.ref().on("child_added",function(childSnapshot){
     console.log(childSnapshot.val());
 
+    //creating a variable and storing the child's name, destination, frequency, first time, 
+    //converted first time. 
     var trainName = childSnapshot.val().name;
     var trainDestination = childSnapshot.val().destination;
     var trainFrequency = childSnapshot.val().frequency;
     var fTime = childSnapshot.val().firstTime;
     var fTimeConverted = moment(fTime, "HH:mm").subtract(1, "years");
-    var cTime = moment();
+    //difference in time between live and converted time
     var timeDif = moment().diff(moment(fTimeConverted), "minutes");
+    //calculating remainder of the time 
     var remainder = timeDif % trainFrequency;
+    //calculating how much time left for next train to arrive
     var timeLeft = trainFrequency - remainder;
+    //adding the time left to current time in minutes.
     var nextTrain = moment().add(timeLeft, "minutes");
 
+    //dynamically creating a table
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDestination),
@@ -86,6 +110,7 @@ database.ref().on("child_added",function(childSnapshot){
         $("<td>").text(timeLeft)
     );
 
+    // append to the table to the screen
     $("#train-table > tbody").append(newRow);
 
 
